fce00f5a3e2b4cbef17d7347d2cdc48b2aff03d2
naty4git <nathanael4wof@gmail.com>
2024-07-30 17:02:57 +0300

    updated navbar and added active state with Navlink

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,33 +11,56 @@ import setting from '../assets/setting.jpg';
 import logout from '../assets/logout.png'
 import profile from '../assets/profile-icon.png';
 import './Sidebar.css';
+import { NavLink } from 'react-router-dom';
 
 function Sidebar({ isVisible, toggleSidebar }) {
+  const items = [
+    { id: 1, label: 'Dashboard', link: '/dashboard', icon: dashboard },
+    { id: 2, label: 'Products', link: '/Products', icon: product },
+    { id: 3, label: 'Notification', link: '/notification', icon: notification },
+    { id: 4, label: 'Verify ID', link: '/verify-id', icon: verify },
+    { id: 5, label: 'Profile', link: '/profile', icon: user },
+    { id: 6, label: 'Setting', link: '/setting', icon: setting },
+  ];
+
   return (
     <div className={`sidebar ${isVisible ? 'visible' : 'hidden'}`}>
       <div className="sidebar-header">
-        <a className='title' href='/'>Prime Connect</a>
+        <a className="title" href="/">
+          Prime Connect
+        </a>
         <div className="header-buttons">
-          <button className='nightmode'><img src={sun} className='sun' alt="sun" /></button>
+          <button className="nightmode">
+            <img src={sun} className="sun" alt="sun" />
+          </button>
           <button className="sidebar-toggle" onClick={toggleSidebar}>
-            <img src={back} className='back' alt="back" />
+            <img src={back} className="back" alt="back" />
           </button>
         </div>
-
       </div>
       <ul className="menu">
-        <li><Link to="/dashboard" className='dashboard'><img src={dashboard} className='dashboard' alt="Dashboard" /> Dashboard</Link></li>
-        <li><Link to="/Products" className='product'><img src={product} className='product' alt="Product" /> Products</Link></li>
-        <li><Link to="/notification" className='notificationn'><img src={notification} className='notification' alt="Notification" /> Notification</Link></li>
-        <li><Link to="/verify-id" className='verifyId'><img src={verify} className='verify' alt="Verify ID" /> Verify ID</Link></li>
-        <li><Link to="/profile" className='profile'><img src={user} className='user' alt="Profile" /> Profile</Link></li>
-        <li><Link to="/setting" className='setting'><img src={setting} className='setting' alt="Setting" /> Setting</Link></li>
+        {items.map((item) => (
+          <li key={item.id} className="sidebar-list-item">
+            <NavLink to={item.link} className="link" activeClassName="active">
+              <img src={item.icon} className="icon" alt={item.label} />
+              {item.label}
+            </NavLink>
+          </li>
+        ))}
       </ul>
       <div className="logOut">
-        <button className="btnLogout"><img src={logout} className='logout' /> Logout</button>
+        <button className="btnLogout">
+          <img src={logout} className="logout-img" /> Logout
+        </button>
       </div>
       <div className="profile">
-        <button className="btnProfile"> <img src={profile} alt="User Avatar" className='userAvatar' /><div className='account'><p className='text'>Name Name </p> <p className='number'> +251-987-654-321 </p></div></button>
+        <button className="btnProfile">
+          <img src={profile} alt="User Avatar" className="userAvatar" />
+          <div className="account">
+            <p className="text">Name Name</p>
+            <p className="number">+251-987-654-321</p>
+          </div>
+        </button>
       </div>
     </div>
   );
