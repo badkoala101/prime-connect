6f0560b57a30cbe747c70d78bb7860e65adbe2c4
Sara Aklilu <saraaklilu3@gmail.com>
2024-08-26 10:34:08 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -20,10 +20,16 @@ import Product from './pages/Product';
 import Faq from './pages/Faq';
 import Documentation from './pages/Documentation';
 import Profile from './pages/Profile';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import AdminLogin from './pages/Admin/AdminLogin';
+import ViewUsers from './pages/Admin/ViewUsers';
+import AddUser from './pages/Admin/AddUsers';
+import EditUser from './pages/Admin/EditUsers';
+
+
 import MichuHome from './pages/coopproducts/Michu/MichuHome';
 import LoanApplication from './pages/coopproducts/Michu/LoanApplication';
 import MichuLoan from './pages/coopproducts/Michu/MichuLoan';
-
 
 const App = () => {
   const isAuthenticated = !!localStorage.getItem('token');
@@ -40,7 +46,8 @@ const App = () => {
           <Route path="/forgot-password" element={<Forgotpsw />} />
           <Route path="/faq" element={<Faq />} />
           <Route path="/product" element={<Product />} />
-          <Route path="/Documentation" element={<Documentation />}/>     
+          <Route path="/documentation" element={<Documentation />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
 
           {/* Protected User Routes */}
           <Route element={<ProtectedRooute isAuthenticated={isAuthenticated} />}>
